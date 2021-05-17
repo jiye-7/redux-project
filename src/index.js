e0f1d5dcff9e3f7d2bda81fe55ce4f5ca491e9ee
jiye-7 <jy522hi@gmail.com>
2021-05-17 23:32:44 +0900

    redux 복습

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ number.innerText = 0;
 const ADD = "ADD";
 const MINUS = "MINUS";
 
-// data를 수정하는 유일한 곳!
-const countModifier = (count = 0, action) => {
-  // action을 통해서 count를 증가, 감소시킬 수 있다.
+const reducer = (count = 0, action) => {
   switch (action.type) {
     case ADD:
       return count + 1;
@@ -20,21 +18,21 @@ const countModifier = (count = 0, action) => {
     default:
       return count;
   }
-};
-
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  console.log(countStore.getState());
-  number.innerText = countStore.getState();
 }
 
-countStore.subscribe(onChange); // subscribe는 store안에 있는 변화들을 알 수 있게 해 준다.
+const store = createStore(reducer);
+
+const numberChange = () => {
+  number.innerText = store.getState();
+}
+
+store.subscribe(numberChange);
+console.log(store)
 
 add.addEventListener('click', () => {
-  countStore.dispatch({ type: ADD })
+  store.dispatch({ type: ADD });
 });
 
 minus.addEventListener('click', () => {
-  countStore.dispatch({ type: MINUS })
+  store.dispatch({ type: MINUS });
 });
